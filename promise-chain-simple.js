a5505f8eaf1fe32f559241ce316f727a358a6e04
fluency03 <fluency.03@gmail.com>
2017-03-20 18:38:51 +0100

    promise-chain

--- promise-chain-simple.js
+++ promise-chain-simple.js
@@ -52,3 +52,23 @@ promise.then(promisedStep1)
   // handle any error from all above steps
   console.log(error);
 });
+
+
+
+
+// Google example
+asyncThing1().then(function() {         // (A)
+  return asyncThing2();
+}).then(function() {                    // (B)
+  return asyncThing3();
+}).catch(function(err) {                // (C)
+  return asyncRecovery1();
+}).then(function() {                    // (D)
+  return asyncThing4();
+}, function(err) {                      // (E)
+  return asyncRecovery2();
+}).catch(function(err) {                // (F)
+  console.log("Don't worry about it");
+}).then(function() {                    // (G)
+  console.log("All done!");
+})
