e3935d0ae4489fc34a97bd7b31a04608908c8a35
fluency03 <fluency.03@gmail.com>
2017-03-20 15:51:38 +0100

    create by resolve or reject

---
+++ create-by-resolve-or-reject.js
@@ -0,0 +1,67 @@
+// value: string
+Promise.resolve('I am Chang!').then(function(value) {
+  console.log(value); // "I am Chang!"
+}, function(error) {
+  // not called
+});
+
+
+// value: array
+Promise.resolve(['I', 'am', 'Chang', '!']).then(function(value) {
+  console.log(value[2]); // "Chang"
+}, function(error) {
+  // not called
+});
+
+
+// promise
+var iAmChang = false;
+
+var promise = new Promise(function(resolve, reject) {
+  // do something
+
+  if (iAmChang) {
+    resolve("You are Chang!"); // value
+  }
+  else {
+    reject(Error("You are not Chang!")); // reason
+  }
+});
+
+Promise.resolve(promise).then(function(value) {
+  console.log(value); // "You are Chang!" when iAmChang == true;
+}, function(error) {
+  console.log(error); // "You are not Chang!" when iAmChang == false;
+});
+
+console.log(Promise.resolve(promise) === promise); // true
+
+
+// thenable1
+var thenable1 = { then: function(resolve) {
+  throw Error('thenable1 Throwing');
+  resolve('thenable1 Resolving');
+}};
+
+
+Promise.resolve(thenable1).then(function(value) {
+  console.log(value);
+}, function(error) {
+  console.log(error); // Error: thenable1 Throwing
+});
+
+console.log(Promise.resolve(thenable1) instanceof Promise); // true
+
+
+// thenable2
+var thenable2 = { then: function(resolve) {
+  resolve('thenable2 Resolving');
+}};
+
+Promise.resolve(thenable2).then(function(value) {
+  console.log(value); // "thenable2 Resolving"
+}, function(error) {
+  console.log(error);
+});
+
+console.log(Promise.resolve(thenable2) instanceof Promise); // true
