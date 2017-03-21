7dd173dc2040f164d10797ed3ac8ebaa5a4e8087
fluency03 <fluency.03@gmail.com>
2017-03-21 10:32:34 +0100

    q-denodeify.js

---
+++ q-denodeify.js
@@ -0,0 +1,41 @@
+var Q = require('q');
+
+var nodeStyleFunc = function(boolValue, intValue, strValue, callback) {
+  if (boolValue) {
+    callback(boolValue, intValue, strValue);
+  } else {
+    callback(null, intValue, strValue);
+  }
+};
+
+
+nodeStyleFunc(true, 1, "denodeify", function(error, result1, result2) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Result: ", result1, result2);
+  }
+});
+
+
+var qStyleFunc = Q.denodeify(nodeStyleFunc);
+
+qStyleFunc(true, 1, "denodeify")
+  .catch(function(error) {
+    console.log(error);
+  })
+  .done(function(result) {
+    console.log("Result: ", result);
+  });
+
+// when boolValue is true:
+//
+// true
+// true
+// Result:  undefined
+
+
+// when boolValue is false:
+//
+// Result:  1 denodeify
+// Result:  [ 1, 'denodeify' ]
